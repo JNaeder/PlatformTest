a1586703bd8acc234c99cf123065896c02794dac
JNaeder <J.naeder324@gmail.com>
2018-06-09 15:42:35 -0400

    Added Things

---
+++ 5-24-18 Controlling Test/Assets/Scripts/Paralax.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour {
+
+	public Transform[] backgrounds;
+	private float[] parallaxScales;
+	public float smoothing = 1f;
+
+	private Transform cam;
+	private Vector3 previousCamPos;
+
+
+	private void Awake()
+	{
+		cam = Camera.main.transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		previousCamPos = cam.position;
+		parallaxScales = new float[backgrounds.Length];
+
+		for (int i = 0; i < backgrounds.Length; i++){
+			parallaxScales[i] = backgrounds[i].position.z * -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < backgrounds.Length; i++){
+			float parallaxX = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+			float backgroundTargetPosX = backgrounds[i].position.x + parallaxX;
+
+			float parallaxY = ((previousCamPos.y - cam.position.y)) * parallaxScales[i];
+
+			float backGroundTargetPosY = backgrounds[i].position.y + parallaxY;
+
+			Vector3 targetPos = new Vector3(backgroundTargetPosX, backGroundTargetPosY, backgrounds[i].position.z);
+
+
+			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, targetPos, smoothing * Time.deltaTime);
+            
+		}
+
+		previousCamPos = cam.position;
+
+
+
+	}
+}
---
+++ 5-24-18 Controlling Test/Assets/Scripts/Tiling.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+
+public class Tiling : MonoBehaviour {
+
+	public int offsetX = 2;
+
+	public bool hasARightBuddy = false;
+	public bool hasALeftBuddy = false;
+
+	public bool reverseScale = false;
+
+	private float spriteWidth = 0f;
+	private Camera cam;
+	private Transform myTransform;
+
+	private void Awake()
+	{
+		cam = Camera.main;
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sRend = GetComponent<SpriteRenderer>();
+		spriteWidth = sRend.sprite.bounds.size.x;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(hasALeftBuddy == false || hasARightBuddy == false){
+			float camHorExtent = cam.orthographicSize * Screen.width / Screen.height;
+
+			float edgeVisRight = (myTransform.position.x + spriteWidth / 2) - camHorExtent;
+			float edgeVisLeft = (myTransform.position.x - spriteWidth / 2) + camHorExtent;
+
+			if(cam.transform.position.x >= edgeVisRight - offsetX && hasARightBuddy == false){
+				MakeNewBuddy(1);
+				hasARightBuddy = true;
+
+			} else if(cam.transform.position.x <= edgeVisLeft + offsetX && hasALeftBuddy == false){
+				MakeNewBuddy(-1);
+				hasALeftBuddy = true;
+
+			}
+            
+		}
+	}
+
+
+
+	void MakeNewBuddy(int rightOfLeft){
+
+		Vector3 newPos = new Vector3(myTransform.position.x + spriteWidth * rightOfLeft, myTransform.position.y, myTransform.position.z);
+
+		Transform newBuddy = Instantiate(myTransform, newPos, myTransform.rotation) as Transform;
+
+		if(reverseScale){
+			newBuddy.localScale = new Vector3(newBuddy.localScale.x * -1, newBuddy.localScale.y, newBuddy.localScale.z);         
+		}
+
+		newBuddy.parent = myTransform.parent;
+
+		if(rightOfLeft > 0){
+			newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+
+		} else if(rightOfLeft < 0){
+			newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+		}
+	}
+}
